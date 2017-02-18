dcc01d743a3fd32ef40392c78e6324cfb8c15cfc
Tanzeem Sayyed <sayyed.tanzeem@gmail.com>
2017-02-18 20:24:01 +0530

    thumbnail images and addition of product

--- gui/js/mock/mock-data-for-product.js
+++ gui/js/mock/mock-data-for-product.js
@@ -5,13 +5,63 @@
 var aProducts = [
     {
         id: "prod1",
-        label: "In order to attain complete client credibility, we offer",
+        label: "Vita-AB Oral Liquid",
         image: "img/old-ui-img/prod1.jpeg"
     },
     {
         id: "prod2",
-        label: "In order to attain complete client credibility, we offer",
+        label: "Tylo-AB Injection",
         image: "img/old-ui-img/prod2.jpeg"
+    },
+    {
+        id: "prod3",
+        label: "Uron-AB Bolus",
+        image: "img/old-ui-img/prod3.jpeg"
+    },
+    {
+        id: "prod4",
+        label: "ATN-3G Powder",
+        image: "img/old-ui-img/prod4.jpeg"
+    },
+    {
+        id: "prod5",
+        label: "Shrical DS Liquid",
+        image: "img/old-ui-img/prod5.jpeg"
+    },
+    {
+        id: "prod6",
+        label: "Flox Oral Solution",
+        image: "img/old-ui-img/prod6.jpeg"
+    },
+    {
+        id: "prod4",
+        label: "ATN-3G Powder",
+        image: "img/old-ui-img/prod4.jpeg"
+    },
+    {
+        id: "prod5",
+        label: "Shrical DS Liquid",
+        image: "img/old-ui-img/prod5.jpeg"
+    },
+    {
+        id: "prod6",
+        label: "Flox Oral Solution",
+        image: "img/old-ui-img/prod6.jpeg"
+    },
+    {
+        id: "prod1",
+        label: "Vita-AB Oral Liquid",
+        image: "img/old-ui-img/prod1.jpeg"
+    },
+    {
+        id: "prod2",
+        label: "Tylo-AB Injection",
+        image: "img/old-ui-img/prod2.jpeg"
+    },
+    {
+        id: "prod3",
+        label: "Uron-AB Bolus",
+        image: "img/old-ui-img/prod3.jpeg"
     }
 ];
 
